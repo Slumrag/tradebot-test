33ab9a5c068e499a8f9a2f0dad37fb19d3ad6ddf
Slumrag <mrgreenthecristal@yandex.ru>
2025-02-24 21:17:56 +0300

    refactor: add icon exports

---
+++ src/components/icons/index.ts
@@ -0,0 +1,7 @@
+export { default as BurgerIcon } from './BurgerIcon';
+export { default as CartIcon } from './CartIcon';
+export { default as ChartIcon } from './ChartIcon';
+export { default as CoinIcon } from './CoinIcon';
+export { default as DashboardIcon } from './DashboardIcon';
+export { default as RefreshIcon } from './RefreshIcon';
+export { default as SettingsIcon } from './SettingsIcon';
